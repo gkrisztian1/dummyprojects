4b5c27ab480326e5f087abef6fa4d37ea1513b80
benceww <benceww@freemail.hu>
2021-10-22 22:56:11 +0200

    path javitva

--- datamappa/file_reader.py
+++ datamappa/file_reader.py
@@ -1,12 +1,14 @@
 import csv
+path = "D:/Documents/GitHub/dummyprojects"
 
-def read_file(path):
+def read_file(filename):
     rows = []
-    csvfile = open(path, mode="r")
+    csvfile = open(path + filename, mode="r")
     file= csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
     for i in file:
         rows.append(i)
     return rows
+    csvfile.close()
 
 print(read_file("/data/42ae8.csv"))
 
